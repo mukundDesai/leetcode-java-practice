2a7852e38108580c03f07bc25e3c7cdfa57e6de7
mukundDesai <mukunddesai153@gmail.com>
2022-05-05 22:27:44 +0530

    05-05-2022

--- Driver.java
+++ Driver.java
@@ -1,8 +1,16 @@
+import java.util.*;
+
 public class Driver {
     public static void main(String[] args) {
-        maxOp obj = new maxOp();
-        int[] nums = { 1, 4 };
-        int k = 5;
-        System.out.println(obj.maxOperations(nums, k));
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        arr.sort(Comparator.naturalOrder());
+
+        System.out.println(arr.get(arr.size() - 1) - arr.get(0) - arr.size());
+        sc.close();
     }
 }
